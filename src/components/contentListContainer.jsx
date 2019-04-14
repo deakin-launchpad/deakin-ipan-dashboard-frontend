29703fc8c767157b970bb7370d5d79a2351d6f62
Nirav Bhimani <niravbhimani53@gmail.com>
2019-04-15 00:22:28 +1000

    contentListContainer: Remove row & col class

--- src/components/contentListContainer.jsx
+++ src/components/contentListContainer.jsx
@@ -3,31 +3,27 @@ import React from 'react'
 export const ContentListContainer = (props) => {
   return (
     <div className="content-list">
-      <div class="row">
-        <div class="col s6 m4 l4">
-          <div class="card">
-            <div class="card-content">
-              <span class="card-title left-align">{props.title}</span>
+      <div class="card">
+        <div class="card-content">
+          <span class="card-title left-align">{props.title}</span>
 
-              <div class="card-action">
-                {
-                  props.data.length > 0 ? (
-                    props.data.map(item => {
-                      return (
-                        <div key= { item.id }>
-                          <p className={"col s11 m11 l11 left-align " + (props.selectedTaskId === item.id ? "active teal lighten-5" : '')} onClick={() => props.onClickAction(item.id)}>
-                            {item.title}
-                          </p>
-                          <i className="material-icons col l1 m1 s1 valign-wrapper">delete</i>
-                        </div>
-                      )
-                    })
-                  ) : <p> Looks empty </p>
-                }
-              </div>
-
-            </div>
+          <div class="card-action">
+            {
+              props.data.length > 0 ? (
+                props.data.map(item => {
+                  return (
+                    <div key= { item.id }>
+                      <p className={"col s11 m11 l11 left-align " + (props.selectedTaskId === item.id ? "active teal lighten-5" : '')} onClick={() => props.onClickAction(item.id)}>
+                        {item.title}
+                      </p>
+                      <i className="material-icons col l1 m1 s1 valign-wrapper">delete</i>
+                    </div>
+                  )
+                })
+              ) : <p> Looks empty </p>
+            }
           </div>
+          
         </div>
       </div>
     </div>
