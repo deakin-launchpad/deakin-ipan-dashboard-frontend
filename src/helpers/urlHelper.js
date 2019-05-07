45d507807dc73773841e1020c522e5b75a85421e
linhtang90 <xltang@edu.deakin.au>
2019-05-07 14:32:12 +1000

    - Remove console log

--- src/helpers/urlHelper.js
+++ src/helpers/urlHelper.js
@@ -1,5 +1,5 @@
 export const replacePlaceHolder = (str, values, placeholder='?') => {
-    console.log("replace holder:" + str);
+    //console.log("replace holder:" + str);
     if (!values) return str;
     for(var i = 0; i < values.length; i++) {
       str = str.replace(placeholder, values[i])
